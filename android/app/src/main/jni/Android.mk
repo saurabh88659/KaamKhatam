3383ae9cf43b5f233b945fa15858959c699aa00e
pcgitreactnative <pcgitreactnative@gmail.com>
2023-05-26 18:54:18 +0530

    rename app

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -10,7 +10,7 @@ include $(CLEAR_VARS)
 LOCAL_PATH := $(THIS_DIR)
 
 # You can customize the name of your application .so file here.
-LOCAL_MODULE := allonone_appmodules
+LOCAL_MODULE := kaamkhatam_appmodules
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
