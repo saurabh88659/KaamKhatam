185e96cbd15cf51fa43aad2c62b6134b4f5d2384
pcgitreactnative <pcgitreactnative@gmail.com>
2023-05-26 18:58:18 +0530

    final code kaamkhatm

--- ios/Kaamkhatam/AppDelegate.mm
+++ ios/Kaamkhatam/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Kaamkhatam", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"kaamkhatam", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
--- ios/KaamkhatamTests/KaamkhatamTests.m
+++ ios/KaamkhatamTests/KaamkhatamTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KaamkhatamTests : XCTestCase
+@interface kaamkhatamTests : XCTestCase
 
 @end
 
-@implementation KaamkhatamTests
+@implementation kaamkhatamTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
