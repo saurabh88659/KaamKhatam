7392af3260991846890fc1d3cecdd16cca954599
pcgitreactnative <pcgitreactnative@gmail.com>
2023-05-26 18:50:03 +0530

    final Update

--- Source/Assets/utils/Restapi/Config.js
+++ Source/Assets/utils/Restapi/Config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://192.168.68.114:5000/user';
-export const BASE_URL = 'https://all.getcloths.co/user';
+// export const BASE_URL = 'https://all.getcloths.co/user';
+export const BASE_URL = 'https://kaamkhatam.com/user';
